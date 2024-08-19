e429801c692b614f412e6b63e2852b7428b50961
Toka136 <145512601+Toka136@users.noreply.github.com>
2024-08-19 18:38:53 +0300

    last2
    last2

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for(var i=1;i<=31;i++)
 {
     day_loop+=start + i + end; 
 }
-for(var i=1960;i<=2024;i++)
+for(var i=1960;i<=2023;i++)
 {
     year_loop+=start + i  + end; 
 }
@@ -26,56 +26,68 @@ for(var i=0;i<months.length;i++)
 day_elm.innerHTML = "<select id='sele'>" + day_loop + "</select>";
 month_elm.innerHTML = "<select id='sele2'>" + month_loop + "</select>";
 year_elm.innerHTML = "<select  id='sele3'>" + year_loop + "</select>";
-
-day2_elm.innerHTML = "<select  id='sele4'>" + day_loop + "</select>";
-month2_elm.innerHTML = "<select id='sele5'>" + month_loop + "</select>";
-year2_elm.innerHTML = "<select  id='sele6'>" + year_loop + "</select>";
 function calc_age()
 {
     var dayinput=document.getElementById("sele").value;
     var monthinput=document.getElementById("sele2").value;
     var yearinput=document.getElementById("sele3").value;
-
-    var day2input=document.getElementById("sele4").value;
-    var month2input=document.getElementById("sele5").value;
-    var year2input=document.getElementById("sele6").value;
-    dayinput=parseInt(dayinput);
-    yearinput=parseInt(yearinput);
-    day2input=parseInt(day2input);
-    year2input=parseInt(year2input);
-    //console.log(typeof(month2input)+" "+typeof( monthinput));
-    var m1=0,m2=0;;
-    for(var i=0; i<months.length;i++)
-    {
-        if(monthinput==months[i])
-        {
-        m1=i+1
-        }
-        if(month2input==months[i])
-        {
-        m2=i+1
-        }
+    switch (monthinput){
+        case "January":
+            monthinput=1;
+            break;
+        case "February":
+            monthinput=2;
+            break;
+        case "March":
+            monthinput=3;
+            break;
+        case "April":
+            monthinput=4;
+            break;
+            case "May":
+            monthinput=5;
+            break;
+        case "June":
+            monthinput=6;
+            break;
+        case "July":
+            monthinput=7;
+            break;
+        case "August":
+            monthinput=8;
+            break;
+        case "September":
+            monthinput=9;
+            break;
+        case "October":
+            monthinput=10;
+            break;
+        case "November":
+            monthinput=11;
+            break;
+        case "December":
+            monthinput=12;
+            break;
+        
     }
-    if(year2input<yearinput || year2input==yearinput)
-        document.getElementById('res').innerHTML=("Current Date Must be Graeter than Birth Date");
-    else{
-    var res1=day2input-dayinput;
-    var res2 =m2-m1;
-    var res3=year2input-yearinput;
-    if(res2<=0)
+const now = new Date();
+const day = now.getDate(); 
+const month = now.getMonth()+1;
+const year = now.getFullYear(); 
+var res1=day-dayinput;
+var res2=month-monthinput;
+var res3 =year-yearinput;
+if(res2<=0)
     {
         res3--;
-        res2=12+res2;
+        res2+=12;
     }
-    
-    if(res1<0)
+if(res1<0)
     {
         res2--;
-        if(m2==1||m2==3||m2==5||m2==7||m2==8||m2==10||m2==12||m1==1||m1==3||m1==5||m1==7||m1==8||m1==10||m1==12);
-        res1++;
-        res1+=30;
+        res1+=31;
     }
-    
-    document.getElementById('res').innerHTML=("You are " + res3 + " years ," + res2 + " Months and "+res1 + " Days")
-    }
+
+
+document.getElementById('res').innerHTML=("You are " + res3 + " years ," + res2 + " Months and "+res1 + " Days")
 }
